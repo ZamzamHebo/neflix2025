5986dcfbf846d60892c7ce3a347d21c524c3f26e
Beki2121 <berekethabtamu2121@gmail.com>
2025-05-10 13:43:11 +0300

    banner

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -3,8 +3,6 @@ import './App.css'
 import Home from './Pages/Home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Home />
---
+++ netflix-clone/src/components/Banner/Banner.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import axios from "../utils/axios";
+import requests from "../utils/requests";
+import "./Banner.css";
+
+function Banner() {
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const request = await axios.get(requests.fetchNetflixOriginals);
+        console.log(requests);
+        setMovie(
+          request.data.results[
+            Math.floor(Math.random() * request.data.results.length)
+          ]
+        );
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const truncate = (string, n) => {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  };
+
+  return (
+    <div
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner_buttons">
+          <button className="banner_button play">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
+      </div>
+      <div className="banner--fadeBottom"></div>
+    </div>
+  );
+}
+
+export default Banner;
